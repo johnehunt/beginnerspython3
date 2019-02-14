483d977f686a142a0d141ff749a79b840dbc455d
John Hunt <john@matraining.com>
2019-02-14 08:23:42 +0000

    Added exercise moving accounts into their own module

---
+++ chapter23/exercises/accounts-app.py
@@ -0,0 +1,23 @@
+import accounts
+
+acc1 = accounts.CurrentAccount('123', 'John', 10.05, 'current', 100.0)
+acc2 = accounts.DepositAccount('345', 'John', 23.55, 'savings', 0.5)
+acc3 = accounts.Account('567', 'Phoebe', 12.45, 'investment')
+
+print(acc1)
+print(acc2)
+print(acc3)
+
+acc1.deposit(23.45)
+acc1.withdraw(12.33)
+print('balance:', acc1.balance)
+
+print('Number of Account instances created:', accounts.Account.instance_count)
+
+try:
+    print('balance:', acc1.balance)
+    acc1.withdraw(300.00)
+    print('balance:', acc1.balance)
+except accounts.BalanceError as e:
+    print('Handling Exception')
+    print(e)
---
+++ chapter23/exercises/accounts.py
@@ -0,0 +1,63 @@
+class Account:
+    """" A class used to represent a type of account """
+
+    instance_count = 0
+
+    @classmethod
+    def increment_instance_count(cls):
+        print('Creating new Account')
+        cls.instance_count += 1
+
+    def __init__(self, account_number, account_holder, opening_balance, account_type):
+        Account.increment_instance_count()
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self._balance = opening_balance
+        self.type = account_type
+
+    def deposit(self, amount):
+        self._balance += amount
+
+    def withdraw(self, amount):
+        self._balance -= amount
+
+    @property
+    def balance(self):
+        """ Provides the current balance """
+        return self._balance
+
+    def __str__(self):
+        return 'Account[' + self.account_number +'] - ' + \
+               self.account_holder + ', ' + self.type + ' account = ' + str(self.balance)
+
+class BalanceError(Exception):
+    """ Valid Ages must be between 0 and 120 """
+
+    def __init__(self, account):
+        self.account = account
+
+class CurrentAccount(Account):
+
+    def __init__(self, account_number, account_holder, opening_balance, account_type, overdraft_limit):
+        super().__init__(account_number, account_holder, opening_balance, account_type)
+        self.overdraft_limit = -overdraft_limit
+
+    def withdraw(self, amount):
+        if self._balance - amount < self.overdraft_limit:
+            print('Withdrawal would exceed your overdraft limit')
+            raise BalanceError(self)
+        else:
+            self._balance -= amount
+
+    def __str__(self):
+        return super().__str__() + ' overdraft limit: ' + str(self.overdraft_limit)
+
+
+class DepositAccount(Account):
+
+    def __init__(self, account_number, account_holder, opening_balance, account_type, interest_rate):
+        super().__init__(account_number, account_holder, opening_balance, account_type)
+        self.interest_rate = interest_rate
+
+    def __str__(self):
+        return super().__str__() + 'interest rate: ' + str(self.interest_rate)
