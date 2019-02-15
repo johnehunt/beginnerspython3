16670bebc556d56de50bb33ca94e0fdbef4de002
John Hunt <john@matraining.com>
2019-02-15 09:29:03 +0000

    Added some comments and print statements to hexagon solution

--- chapter33/exercises/draw-hexagons.py
+++ chapter33/exercises/draw-hexagons.py
@@ -1,20 +1,26 @@
 import turtle
 
+
+# Function to draw a hexagon
 def hexagon():
     for _ in range(6):
         turtle.forward(50)
         turtle.left(60)
 
+print('Set up')
+# Setup window
 turtle.title('Hexagons')
 turtle.setup(200, 200, 0, 0)
 turtle.speed(10)
 turtle.hideturtle()
-
 turtle.pencolor('blue')
 
-for _ in range (6):
+print('Start to draw hexagons')
+# Draw six hexagons
+for _ in range(6):
     hexagon()
     turtle.forward(50)
     turtle.right(60)
 
+print('Done')
 turtle.done()
