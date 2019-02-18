99d4a721a0aeb32b860f2d0d79e1c5ede656e63d
John Hunt <john@matraining.com>
2019-02-18 18:00:04 +0000

    Removed unused variable

--- chapter39/future7.py
+++ chapter39/future7.py
@@ -1,10 +1,10 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
 from time import sleep
 from random import randint
 
 
 def is_even(n):
-    print('Checking if', n , 'is even')
+    print('Checking if', n, 'is even')
     sleep(randint(1, 5))
     return str(n) + ' ' + str(n % 2 == 0)
 
@@ -17,7 +17,6 @@ print('Started')
 data = [1, 2, 3, 4, 5, 6]
 
 pool = ThreadPoolExecutor(5)
-futures = []
 
 for v in data:
     future = pool.submit(is_even, v)
