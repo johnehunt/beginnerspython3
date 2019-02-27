9867a05666c7037f68315544cd946ff40cf4d488
John Hunt <john@matraining.com>
2019-02-27 09:27:59 +0000

    Removed unused import and added further comments

--- chapter33/fractal_trees.py
+++ chapter33/fractal_trees.py
@@ -1,27 +1,12 @@
 import turtle
-import math
+
+# Set up 'constants'
 
 # image size
 IMAGE_SIZE_X = 500
 IMAGE_SIZE_Y = 500
-
+# Determines depth of tree - try 2 and 1.25 as alternatives
 FACTOR = 1.45
-
-
-def setup_screen(title, background='white', screen_size_x=640, screen_size_y=320, tracer_size=200):
-    print('Set up Screen')
-    turtle.title(title)
-    turtle.setup(screen_size_x, screen_size_y)
-    turtle.hideturtle()
-    turtle.penup()
-    turtle.backward(240)
-    turtle.tracer(tracer_size)
-    turtle.bgcolor(background)  # Set the background colour of the screen
-
-
-setup_screen('Fractal Tree', screen_size_x=IMAGE_SIZE_X, screen_size_y=IMAGE_SIZE_Y, tracer_size=2000)
-turtle.colormode(255)  # Indicates RGB numbers will be in the range 0 to 255
-turtle.left(90)
 
 
 def draw_tree(length, width=9):
@@ -47,10 +32,27 @@ def draw_tree(length, width=9):
     turtle.backward(length)
 
 
-# Ensure tree is centred
-turtle.setposition(-50, -140)
+def setup_screen(title, background='white', screen_size_x=640, screen_size_y=320, tracer_size=200):
+    """ Sets up Turtle screen with useful defaults """
+    print('Set up Screen')
+    turtle.title(title)
+    turtle.setup(screen_size_x, screen_size_y)
+    turtle.hideturtle()
+    turtle.penup()
+    turtle.backward(240)
+    turtle.tracer(tracer_size)
+    turtle.bgcolor(background)  # Set the background colour of the screen
 
+
+print("Starting ...")
+setup_screen('Fractal Tree', screen_size_x=IMAGE_SIZE_X, screen_size_y=IMAGE_SIZE_Y, tracer_size=2000)
+
+# Ensure tree is centred and point in the right direction
+turtle.setposition(-50, -140)
+turtle.left(90)
 turtle.down()
+
+# Draw the tree
 draw_tree(100)
 
 # Ensure that all the drawing is rendered
