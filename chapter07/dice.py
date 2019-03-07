29da446f7c1ac0dad381514d37aaae13c18bb981
John Hunt <john@matraining.com>
2019-03-07 15:05:00 +0000

    CHanged global 'constants' to use upper case

--- chapter07/dice.py
+++ chapter07/dice.py
@@ -1,15 +1,15 @@
 import random
 
-min = 1
-max = 6
+MIN = 1
+MAX = 6
 
 roll_again = 'y'
 
 while roll_again == 'y':
     print('Rolling the dices...')
     print('The values are....')
-    dice1 = random.randint(min, max)
+    dice1 = random.randint(MIN, MAX)
     print(dice1)
-    dice2 = random.randint(min, max)
+    dice2 = random.randint(MIN, MAX)
     print(dice2)
     roll_again = input('Roll the dices again? (y / n): ')
