61b61a5ce9cbd9ace511f4157a4895647bc3cb0c
John Hunt <john@matraining.com>
2019-08-07 10:02:31 +0100

    Added a decorator with parameters example

---
+++ chapter29/parameterized_decorator.py
@@ -0,0 +1,28 @@
+def register(active=True):
+    def wrap(func):
+        def wrapper():
+            print('Calling ', func.__name__, ' decorator param ', active)
+            if active:
+                func()
+                print('Called ', func.__name__)
+            else:
+                print('Skipped ', func.__name__)
+
+        return wrapper
+
+    return wrap
+
+
+@register()
+def func1():
+    print('func1')
+
+
+@register(active=False)
+def func2():
+    print('func2')
+
+
+func1()
+print('-' * 10)
+func2()
