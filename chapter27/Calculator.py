64e349bf6dc751f08d8fcdd8a8d6ce9952ba19ee
John Hunt <jeh@midmarsh.co.uk>
2020-05-04 21:26:24 +0100

    Illustrates adding two strings

--- chapter27/Calculator.py
+++ chapter27/Calculator.py
@@ -38,6 +38,7 @@ print('calc.add(3, 4.5):', calc.add(3, 4.5))
 print('calc.add(4.5, 6.2):', calc.add(4.5, 6.2))
 print('calc.add(2.3, 7):', calc.add(2.3, 7))
 print('calc.add(-1, 4):', calc.add(-1, 4))
+print("calc.add('John', 'Hunt')", calc.add('John', 'Hunt'))
 
 print('-' * 25)
 
