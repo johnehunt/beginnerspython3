eae55efca3a245899bab43229e8a8a53c05d30a4
John Hunt <jeh@midmarsh.co.uk>
2020-05-06 10:18:06 +0100

    Example of using arrays

---
+++ chapter31/array_example.py
@@ -0,0 +1,26 @@
+""" Example ofi using Python array objkects - which are space efficient and fast
+for iterative type processing. """
+
+import array as arr
+
+# Create an array Object
+a = arr.array("I",[3,6,9])
+print(a)
+print('len(a):', len(a))
+print(type(a))
+
+# Append an element to the end of the array
+a.append(4)
+print(a)
+
+print("First element:", a[0])
+a[0] = 7
+print("First element:", a[0])
+
+# Remove element form an array
+del a[0]
+print(a)
+
+# Loop through contents of array
+for i in a:
+    print(i)
