594b4626bd72513344b7d5666363ceedb21ad33c
John Hunt <john.hunt10@gmail.com>
2020-10-28 16:27:47 +0000

    Added additional examples of reduce

--- chapter36/reduce-examples.py
+++ chapter36/reduce-examples.py
@@ -1,8 +1,16 @@
 from Person import Person
+
+# Note must import reduce from functools module in Python 3
 from functools import reduce
 
 data = [1, 3, 5, 2, 7, 4, 10]
 result = reduce(lambda total, value: total + value, data)
+print(result)
+
+def generate_total(running_total, value):
+    return running_total + value
+
+result = reduce(generate_total, data)
 print(result)
 
 data = [Person('John', 54), Person('Phoebe', 21), Person('Adam', 19)]
