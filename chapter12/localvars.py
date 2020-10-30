98fcd4987c46f26eef1289b87df6d68172f36388
John Hunt <john.hunt10@gmail.com>
2020-10-30 14:46:49 +0000

    Added soem additional prints to local vars v globals and nonlocal variables

--- chapter12/localvars.py
+++ chapter12/localvars.py
@@ -1,25 +1,38 @@
+a_global_count = 10
+
+def some_func():
+    a_local_variable = 100
+    print('a_local_variable:', a_local_variable)
+    print('a_global_count:', a_global_count)
+
+
+some_func()
+
 def my_function():
     a_variable = 100
-    print(a_variable)
+    print('inside function:', a_variable)
 
 
 a_variable = 25
 my_function()
-print(a_variable)
+print('outside function:', a_variable)
 
 max = 100
+print('initial value of max:', max)
 def print_max():
     global max
     max = max + 1
-    print(max)
+    print('inside function:', max)
 
 
 print_max()
-print(max)
+print('outside function:', max)
 
 
 def outer():
     title = "original title"
+
+    print('initial title:', title)
 
     def inner():
         nonlocal title
